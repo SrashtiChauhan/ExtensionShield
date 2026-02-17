c8ef9816986b209010fb19e288fe4e336f071cad
Stanzin <snorzang65@gmail.com>
2026-02-16 21:57:22 -0600

    Honey animation ring removed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -536,8 +536,6 @@ const HomePage = () => {
                 {/* Animated color rings */}
                 <div className="honey-ring honey-ring-1" />
                 <div className="honey-ring honey-ring-2" />
-                <div className="honey-ring honey-ring-3" />
-                <div className="honey-ring honey-ring-4" />
                 
                 {/* Honey Logo - Hexagon with honeycomb pattern */}
                 <div className="honey-logo">
